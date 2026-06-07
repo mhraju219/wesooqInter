4d17f13e47a8cbc01dbe1656e670ded105954e03
mhraju219 <mhraju219@gmail.com>
2026-06-07 09:42:49 +0300

    Initial commit - change productID

---
+++ src/app/(storefront)/[businessSlug]/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+import { notFound } from 'next/navigation';
+
+export default function ProductDetailPage() {
+  // This page is optional – you can implement product detail view later.
+  notFound();
+}
